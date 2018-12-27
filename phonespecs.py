ee94b6e8076618adf05f0d84b31e667bbc198169
Chris Bunney <chris@hivesource.io>
2018-12-27 01:45:22 +0000

    1st cut on working version that uses hard coded URLs and a fixed set of fields. Prints tab separated values to stdout. Good enough for what I need right now!

--- phonespecs.py
+++ phonespecs.py
@@ -5,14 +5,32 @@ import enum
 
 # take list of phones
 # get webpage for each phone
+import requests
 from bs4 import BeautifulSoup
 
 
 class Field(enum.Enum):
+    BATTERY_TECH = 'battype-hl'
+    BATTERY_SIZE = 'batsize-hl'
+    # CARD_SLOT = ''  # contained in storage text
     CHIPSET = 'chipset'
+    CAMERA_FRONT = 'cam2modules'
+    CAMERA_MAIN = 'camerapixels-hl'
+    MODEL = 'modelname'
+    PRICE = 'price'
+    RAM = 'ramsize-hl'
+    RELEASED = 'released-hl'
+    RESOLUTION = 'displayres-hl'
+    SIZE = 'displaysize-hl'
+    STORAGE = 'storage-hl'
+
+    # URL = ''  # use canonical from <head>
 
     def __str__(self):
         return self.value
+
+    def __repr__(self):
+        return 'Field.' + self.name
 
 
 def search_phone_urls(phone_names: List[str]) -> Dict[str, str]:
@@ -24,14 +42,72 @@ def search_phone_url(phone_name: str) -> str:
 
 
 def get_phone_html(url: str) -> str:
-    return ''
+    return requests.get(url).content
 
 
 def parse_phone_html(html: str, fields: List[Field]) -> Dict[Field, str]:
     parser = BeautifulSoup(html, features='html.parser')
-    val = parser.select('[data-spec="{}"]'.format(fields[0]))[0].get_text()
-    return {fields[0]:val}
+    results = {}
+    for field in fields:
+        results[field] = parser.select('[data-spec="{}"]'.format(field))[0].get_text()
+
+    # cleanup results
+    if Field.CAMERA_FRONT in results:
+        results[Field.CAMERA_FRONT] = results[Field.CAMERA_FRONT].split(' ')[0]
+    if Field.PRICE in results:
+        price = results[Field.PRICE].split(' ')
+        results[Field.PRICE] = '~{}{}'.format(SYMBOLS[price[2]], price[1])
+    return results
+
+
+SYMBOLS = {
+    'EUR': '€',
+    'GBP': '£',
+    'USD': '$',
+}
+
+
+def export_csv(rows: List[Dict[Field, str]], sep='\t'):
+    field_order = [
+        Field.MODEL, Field.PRICE, Field.RELEASED, Field.SIZE, Field.RESOLUTION, Field.CHIPSET, Field.RAM,
+        Field.CAMERA_MAIN, Field.CAMERA_FRONT, Field.BATTERY_TECH, Field.BATTERY_SIZE, Field.STORAGE,
+        # Field.CARD_SLOT, Field.URL
+    ]
+    # Print headings
+    print(sep.join(f.name for f in field_order))
+
+    # Print rows
+    for row in sorted(rows, key=lambda r: r[Field.MODEL]):
+        print(sep.join(row[f] for f in field_order))
+
 
 # search gsmarena for phone
 # parse html for specs
 # format specs and print to CSV
+PHONES = [
+    'https://www.gsmarena.com/apple_iphone_6s-7242.php',
+    'https://www.gsmarena.com/apple_iphone_x-8858.php',
+    'https://www.gsmarena.com/lg_nexus_5-5705.php',
+    'https://www.gsmarena.com/google_pixel-8346.php',
+    'https://www.gsmarena.com/google_pixel_2-8733.php',
+    'https://www.gsmarena.com/google_pixel_3-9256.php',
+    'https://www.gsmarena.com/lg_g7_one-9304.php',
+    'https://www.gsmarena.com/lg_g7_fit-9305.php',
+    'https://www.gsmarena.com/lg_g7_thinq-9115.php',
+    'https://www.gsmarena.com/nokia_8_1_(nokia_x7)-9342.php',
+    'https://www.gsmarena.com/samsung_galaxy_j5-7184.php',
+    'https://www.gsmarena.com/samsung_galaxy_s7-7821.php',
+    'https://www.gsmarena.com/samsung_galaxy_s9-8966.php',
+
+]
+
+
+def do_main():
+    results = []
+    for p in PHONES:
+        results.append(parse_phone_html(get_phone_html(p), list(Field)))
+    export_csv(results)
+
+
+if __name__ == '__main__':
+    do_main()
--- tests/test_phonespecs.py
+++ tests/test_phonespecs.py
@@ -5,9 +5,77 @@ from phonespecs import parse_phone_html, Field
 
 class ParsePhoneHtmlTest(TestCase):
 
-    def test_parse_phone_html(self):
-        with open('google_pixel_2-8733.php', 'r', encoding='iso-8859-1') as testin:
-            testout = parse_phone_html(testin.read(), [Field.CHIPSET])
+    @classmethod
+    def setUpClass(cls):
+        with open('google_pixel_2-8733.html', 'r', encoding='iso-8859-1') as fixture_file:
+            cls.TESTIN = fixture_file.read()
 
+    def test_battery_cap(self):
+        testout = parse_phone_html(self.TESTIN, [Field.BATTERY_SIZE])
+        self.assertDictEqual(testout, {Field.BATTERY_SIZE: '2700'}, msg="Parsed output didn't match expected")
+
+    def test_battery_tech(self):
+        testout = parse_phone_html(self.TESTIN, [Field.BATTERY_TECH])
+        self.assertDictEqual(testout, {Field.BATTERY_TECH: 'Li-Ion'}, msg="Parsed output didn't match expected")
+
+    def test_camera_main(self):
+        testout = parse_phone_html(self.TESTIN, [Field.CAMERA_MAIN])
+        self.assertDictEqual(testout, {Field.CAMERA_MAIN: '12'}, msg="Parsed output didn't match expected")
+
+    def test_camera_front(self):
+        testout = parse_phone_html(self.TESTIN, [Field.CAMERA_FRONT])
+        self.assertDictEqual(testout, {Field.CAMERA_FRONT: '8'}, msg="Parsed output didn't match expected")
+
+    def test_chipset(self):
+        testout = parse_phone_html(self.TESTIN, [Field.CHIPSET])
         self.assertDictEqual(testout, {Field.CHIPSET: 'Qualcomm MSM8998 Snapdragon 835 (10 nm)'},
                              msg="Parsed output didn't match expected")
+
+    def test_model(self):
+        testout = parse_phone_html(self.TESTIN, [Field.MODEL])
+        self.assertDictEqual(testout, {Field.MODEL: 'Google Pixel 2'}, msg="Parsed output didn't match expected")
+
+    def test_price(self):
+        testout = parse_phone_html(self.TESTIN, [Field.PRICE])
+        self.assertDictEqual(testout, {Field.PRICE: '~€550'}, msg="Parsed output didn't match expected")
+
+    def test_ram(self):
+        testout = parse_phone_html(self.TESTIN, [Field.RAM])
+        self.assertDictEqual(testout, {Field.RAM: '4'}, msg="Parsed output didn't match expected")
+
+    def test_released(self):
+        testout = parse_phone_html(self.TESTIN, [Field.RELEASED])
+        self.assertDictEqual(
+            testout, {Field.RELEASED: 'Released 2017, October'}, msg="Parsed output didn't match expected")
+
+    def test_resolution(self):
+        testout = parse_phone_html(self.TESTIN, [Field.RESOLUTION])
+        self.assertDictEqual(testout, {Field.RESOLUTION: '1080x1920 pixels'}, msg="Parsed output didn't match expected")
+
+    def test_size(self):
+        testout = parse_phone_html(self.TESTIN, [Field.SIZE])
+        self.assertDictEqual(testout, {Field.SIZE: '5.0"'}, msg="Parsed output didn't match expected")
+
+    def test_storage(self):
+        testout = parse_phone_html(self.TESTIN, [Field.STORAGE])
+        self.assertDictEqual(
+            testout, {Field.STORAGE: '64/128GB storage, no card slot'}, msg="Parsed output didn't match expected")
+
+    def test_all(self):
+        testout = parse_phone_html(self.TESTIN, list(Field))
+
+        expected = {
+            Field.BATTERY_SIZE: '2700',
+            Field.BATTERY_TECH: 'Li-Ion',
+            Field.CAMERA_MAIN: '12',
+            Field.CAMERA_FRONT: '8',
+            Field.CHIPSET: 'Qualcomm MSM8998 Snapdragon 835 (10 nm)',
+            Field.MODEL: 'Google Pixel 2',
+            Field.PRICE: '~€550',
+            Field.RAM: '4',
+            Field.RELEASED: 'Released 2017, October',
+            Field.RESOLUTION: '1080x1920 pixels',
+            Field.SIZE: '5.0"',
+            Field.STORAGE: '64/128GB storage, no card slot',
+        }
+        self.assertDictEqual(testout, expected, msg="Parsed output didn't match expected")
